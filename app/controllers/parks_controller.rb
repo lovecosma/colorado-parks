a64ad43eb8b9d2ee7cc070cdc6a16a3d193a66aa
love.cosma <williamsmatthew333@gmail.com>
2021-12-01 10:20:26 -0500

    first heroku commit.

---
+++ app/controllers/parks_controller.rb
@@ -0,0 +1,10 @@
+class ParksController < ApplicationController
+    def index 
+       json: Park.all
+    end
+
+    def show
+        park = Park.find(params[:id])
+        json: park
+    end
+end
